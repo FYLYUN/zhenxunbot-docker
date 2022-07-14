e9b3b702425719fa91bbc83bb68636228dd38916
FYLYUN <tianpapawo@live.com>
2022-07-14 22:02:45 +0800

    修改了一些功能

--- files/config.sh
+++ files/config.sh
@@ -12,59 +12,80 @@
 set -e
 
 #只是判断了，没有做出回应，等一个有缘人帮我完善
-if [ ! "$bot_qq" ];then
-    echo -e "\033[31m未设置 bot 使用的 QQ 号\033[0m"
-fi
-if [ ! "$bot_qq_key" ];then
-    echo -e "\033[31m未设置 bot 的 QQ 号密码\033[0m"
-fi
-if [ ! "$admin_qq" ];then
+#if [ ! "$bot_qq" ];then
+#    echo -e "\033[31m未设置 bot 使用的 QQ 号\033[0m"
+#fi
+#if [ ! "$bot_qq_key" ];then
+#    echo -e "\033[31m未设置 bot 的 QQ 号密码\033[0m"
+#fi
+if [ ! "$SU" ];then
     echo -e "\033[31m未设置超级管理员 QQ 号\033[0m"
 fi
+##设置数据库相关内容
+if [ ! "$DB" ];then
+    echo -e "\033[31m未设置数据库链接,设置为默认值\033[0m"
+	DB="postgresql://meimei:meiboli@127.0.0.1:5432/zhenxun"
+fi
+if [ ! "$DB_ROOT" ];then
+    echo -e "\033[31m未设置数据库ROOT密码,设置为默认值\033[0m"
+	DB_ROOT=sdfSDF45sdfg45SD
+fi
+ip=${DB#*@}
+ip=${ip%%:*}
+up=${DB#*//}
+up=${up%%@*}
+user=${up%%:*}
+pw=${up#*:}
+db_name=${DB#*5432/}
 
-sed -i 's/xxxxxx/'$bot_qq'/g' ./go-cqhttp/config.yml
-echo -e "已设置BOT的QQ号为"$bot_qq
-sed -i 's/yyyyyy/'$bot_qq_key'/g' ./go-cqhttp/config.yml
-echo -e "已设置BOT的QQ密码为"$bot_qq_key
-sed -i 's/qqqqqq/'$api_key'/g' ./zhenxun_bot/configs/config.yaml
-echo -e "已设置识图API-KEY为"$api_key
-sed -i 's/root/'$webui_user'/g' ./zhenxun_bot/configs/config.yaml
-echo -e "已设置WebUI的用户名为"$webui_user
-sed -i 's/123456/'$webui_passwd'/g' ./zhenxun_bot/configs/config.yaml
-echo -e "已设置WebUI的密码为"$webui_passwd
-sed -i '3c SUPERUSERS=["'$admin_qq'"]' ./zhenxun_bot/.env.dev
-echo -e "已设置超级管理员的QQ为"$admin_qq
+#sed -i 's/xxxxxx/'$bot_qq'/g' /home/go-cqhttp/config.yml
+#echo -e "已设置BOT的QQ号为"$bot_qq
+#sed -i 's/yyyyyy/'$bot_qq_key'/g' /home/go-cqhttp/config.yml
+#echo -e "已设置BOT的QQ密码为"$bot_qq_key
+sed -i '3c SUPERUSERS=["'$SU'"]' /home/zhenxun_bot/.env.dev
+echo -e "已设置超级管理员的QQ为"$SU
+sed -i 's|bind: str = \"\"|''bind: str = \"'$DB'\"|g' /home/zhenxun_bot/configs/config.py
+echo -e "已设置数据库链接为"$DB
+echo -e "请确保你的数据库信息没有问题，否则会连不上数据库！"
+#因为变量里有/，所以不能用/分隔符，否则会报错，故换成|分隔符
 
-mkdir ./zhenxun_bot/my_plugins >/dev/null 2>&1
-sed -i '14a nonebot.load_plugins("my_plugins")' ./zhenxun_bot/bot.py
-echo -e "已添加自定义插件目录配置"
+#mkdir /home/zhenxun_bot/extensive_plugin >/dev/null 2>&1
+#sed -i '14a nonebot.load_plugins("extensive_plugin")' ./zhenxun_bot/bot.py
+#echo -e "已添加自定义插件目录 extensive_plugin 配置"
 
 get_arch=`arch`
 if [[ $get_arch =~ "x86_64" ]];then
-    mv ./go-cqhttp/go-cqhttp-amd64 ./go-cqhttp/go-cqhttp
-    rm -f ./go-cqhttp/go-cqhttp-arm64
+	if [ -f "/home/go-cqhttp/go-cqhttp-amd64" ];then
+		mv /home/go-cqhttp/go-cqhttp-amd64 /home/go-cqhttp/go-cqhttp
+		rm -f /home/go-cqhttp/go-cqhttp-arm64
+	fi
     echo -e "\033[34m检测到你的设备是amd64架构\033[0m"
 elif [[ $get_arch =~ "aarch64" ]];then
-    mv ./go-cqhttp/go-cqhttp-arm64 ./go-cqhttp/go-cqhttp
-    rm -f ./go-cqhttp/go-cqhttp-amd64
+	if [ -f "/home/go-cqhttp/go-cqhttp-arm64" ];then
+		mv /home/go-cqhttp/go-cqhttp-arm64 /home/go-cqhttp/go-cqhttp
+		rm -f /home/go-cqhttp/go-cqhttp-amd64
+	fi
     echo -e "\033[34m检测到你的设备是arm64架构\033[0m"
 fi
 
 cat > /tmp/sql.sql <<-EOF
-CREATE USER uname WITH PASSWORD 'zhenxun';
-CREATE DATABASE testdb OWNER uname;
-CREATE ROLE root superuser PASSWORD '123456' login;
+CREATE USER $user WITH PASSWORD '$pw';
+CREATE DATABASE $db_name OWNER $user;
+CREATE ROLE root superuser PASSWORD '$DB_ROOT' login;
 EOF
 
 /etc/init.d/postgresql start >/dev/null 2>&1
 su postgres -c "psql -f /tmp/sql.sql" >/dev/null 2>&1
 echo -e "\033[34m已创建数据库\033[0m"
 
-if [ -f "./zhenxun_bot/my_plugins/zhenxun.sql" ]; then
+if [ -f "/home/zhenxun_bot/extensive_plugin/zhenxun.sql" ]; then
     echo -e "检测到数据库文件,开始导入"
     sleep 2s
-    su postgres -c "psql -U postgres testdb < ./zhenxun_bot/my_plugins/zhenxun.sql" >/dev/null 2>&1
+	psql zhenxun < /home/zhenxun_bot/extensive_plugin/zhenxun.sql >/dev/null 2>&1
+#    su postgres -c "psql -U $user -d $db_name < /home/zhenxun_bot/extensive_plugin/zhenxun.sql" >/dev/null 2>&1
     echo -e "\033[32m✔已导入数据库\033[0m"
+	nowtime=$(date "+%Y-%m-%d-%H-%M-%S")
+	mv /home/zhenxun_bot/extensive_plugin/zhenxun.sql /home/zhenxun_bot/extensive_plugin/zhenxun_"$nowtime".sql
 else 
     echo -e "\033[33m没有检测到数据库文件,不导入旧数据库\033[0m"
 fi
